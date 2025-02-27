adffcdb58aad9bbb4c7749c2fe5fa762f31f913a
Valeria <219102@kisit.kneu.edu.ua>
2025-02-28 00:41:45 +0200

    Modifies other

--- src/CineVault.API/Program.cs
+++ src/CineVault.API/Program.cs
@@ -1,5 +1,7 @@
+using Asp.Versioning;
 using CineVault.API.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 [assembly: ApiController]
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,14 +10,36 @@ builder.Services.AddCineVaultDbContext(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "CineVault API", Version = "1" });
+    options.SwaggerDoc("v2", new OpenApiInfo { Title = "CineVault API", Version = "2" });
+});
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+})
+    .AddMvc()
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'V";
+        options.SubstituteApiVersionInUrl = true;
+    });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("v1/swagger.json", "first version");
+        options.SwaggerEndpoint("v2/swagger.json", "second version");
+    });
 }
 
 if (app.Environment.IsLocal())
