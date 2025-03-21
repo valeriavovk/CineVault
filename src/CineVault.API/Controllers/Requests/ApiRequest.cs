f7567a79d345e399e01b5e4044620ee536538276
Valeria <219102@kisit.kneu.edu.ua>
2025-03-22 01:58:35 +0200

    Add unified classes

---
+++ src/CineVault.API/Controllers/Requests/ApiRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace CineVault.API.Controllers.Requests;
+
+public class ApiRequest
+{
+    public required string Username { get; set; }
+    public required string SecretCode { get; set; }
+    public required Dictionary<string, string> AdditionalProperties { get; set; }
+    public required string NameOfServer { get; set; }
+    public Guid RequestId { get; } = Guid.NewGuid();
+}
+
+public class ApiRequest<T> : ApiRequest
+{
+    public required T Data { get; set; }
+}
