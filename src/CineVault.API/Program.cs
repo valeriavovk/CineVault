85673d82a026548b9144e9aa0c8949f8171d827f
Valeria <219102@kisit.kneu.edu.ua>
2025-03-22 02:17:20 +0200

    Modify controllers

--- src/CineVault.API/Program.cs
+++ src/CineVault.API/Program.cs
@@ -1,5 +1,7 @@
+using Asp.Versioning;
 using CineVault.API.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 using Serilog;
 using Serilog.Debugging;
 using Serilog.Events;
@@ -11,7 +13,27 @@ builder.Services.AddCineVaultDbContext(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "CineVault API", Version = "1" });
+    options.SwaggerDoc("v2", new OpenApiInfo { Title = "CineVault API", Version = "2" });
+});
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+})
+    .AddMvc()
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'V";
+        options.SubstituteApiVersionInUrl = true;
+    });
+
 builder.Services.AddScoped<TimerMiddleware>();
 
 builder.Services.AddSerilog(configuration =>
@@ -35,7 +57,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("v1/swagger.json", "v1");
+        options.SwaggerEndpoint("v2/swagger.json", "v2");
+    });
 }
 
 if (app.Environment.IsLocal())
